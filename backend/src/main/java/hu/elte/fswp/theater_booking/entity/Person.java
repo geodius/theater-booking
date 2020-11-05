a0127a9c05aa18a99ada30652f38f03e2a619983
Geodius <saturnusfox@gmail.com>
2020-11-05 16:34:21 +0100

    Added password field

--- backend/src/main/java/hu/elte/fswp/theater_booking/entity/Person.java
+++ backend/src/main/java/hu/elte/fswp/theater_booking/entity/Person.java
@@ -16,6 +16,7 @@ public class Person {
     private int id;
     private String name;
     private String email;
+    private String password;
 
     @OneToMany(mappedBy = "person", cascade = CascadeType.ALL)
     private List<Reservation> reservations;
