c9b4b3ce36667b8dd904d60c8305d1b78d31c250
Geodius <saturnusfox@gmail.com>
2020-11-05 17:44:17 +0100

    Added Room CRUD features

---
+++ backend/src/main/java/hu/elte/fswp/theater_booking/controller/RoomController.java
@@ -0,0 +1,43 @@
+package hu.elte.fswp.theater_booking.controller;
+
+import hu.elte.fswp.theater_booking.entity.Room;
+import hu.elte.fswp.theater_booking.model.RoomModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class RoomController {
+    @PutMapping("/room/create")
+    public ResponseEntity<Room> create(@RequestBody Room room){
+        Optional<Room> result = RoomModel.getInstance().create(room);
+        return result.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build());
+    }
+
+    @GetMapping("/room/getAll")
+    public ResponseEntity<List<Room>> getAll(){
+        List<Room> results = RoomModel.getInstance().getAll();
+        return ResponseEntity.ok(results);
+    }
+
+    @GetMapping("/room/getByName")
+    public ResponseEntity<Optional<Room>> getByName(@RequestBody String name){
+        Optional<Room> results = RoomModel.getInstance().getByName(name);
+        return ResponseEntity.ok(results);
+    }
+
+    @PatchMapping("/room/modify")
+    public ResponseEntity<Room> modify(@RequestBody Room room) {
+        Optional<Room> result = RoomModel.getInstance().modify(room);
+        return result.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_MODIFIED).build());
+    }
+
+    @DeleteMapping("/room/delete")
+    public ResponseEntity<Boolean> delete(@RequestBody Room room) {
+        boolean result = RoomModel.getInstance().delete(room);
+        return result ? ResponseEntity.ok(true) : ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+}
---
+++ backend/src/main/java/hu/elte/fswp/theater_booking/model/RoomModel.java
@@ -0,0 +1,57 @@
+package hu.elte.fswp.theater_booking.model;
+
+import hu.elte.fswp.theater_booking.database.RoomRepo;
+import hu.elte.fswp.theater_booking.entity.Room;
+import hu.elte.fswp.theater_booking.utility.Utility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RoomModel {
+    public static RoomModel instance;
+
+    public static RoomModel getInstance() {
+        assert(instance != null);
+        return instance;
+    }
+
+    private final RoomRepo roomRepo;
+
+    @Autowired
+    public RoomModel(RoomRepo roomRepo) {
+        assert(instance == null);
+        instance = this;
+        this.roomRepo = roomRepo;
+    }
+
+    public Optional<Room> create(Room room) {
+        if (!Room.isRoomValid(room)) return Optional.empty();
+        if (roomRepo.findById(room.getId()).isPresent()) return Optional.empty();
+        roomRepo.save(room);
+        return roomRepo.findByName(room.getName());
+    }
+
+    public Optional<Room> getByName(String name) {
+        return roomRepo.findByName(name);
+    }
+
+    public List<Room> getAll() {
+        return Utility.ConvertIterableToList(roomRepo.findAll());
+    }
+
+    public Optional<Room> modify(Room room) {
+        if (!Room.isRoomValid(room)) return Optional.empty();
+        if (roomRepo.findById(room.getId()).isEmpty()) return Optional.empty();
+        roomRepo.save(room);
+        return roomRepo.findById(room.getId());
+    }
+
+    public boolean delete(Room room) {
+        if (!roomRepo.existsById(room.getId())) return false;
+        roomRepo.delete(room);
+        return true;
+    }
+}
